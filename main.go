d90fa2dd3142ba1347498be06c9735be8fb02d87
radovancemes <radovan.cemes@gmail.com>
2024-10-30 18:42:58 +0100

    Create main.go

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+func newMux() http.Handler {
+	m := http.NewServeMux()
+
+	m.HandleFunc("/", rootHandler)
+
+	return m
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("%s request received: %s:%s:%s\n", r.Method, r.UserAgent(), r.URL, r.RemoteAddr)
+	hostname, err := os.Hostname()
+	if err != nil {
+		http.Error(w, "Error getting server details", http.StatusInternalServerError)
+		return
+	}
+
+	prefix := "API"
+
+	if customPrefix := os.Getenv("CUSTOM_PREFIX"); customPrefix != "" {
+		prefix = customPrefix
+	}
+
+	fmt.Fprintf(w, "%s: This request is being served by server %s\n", prefix, hostname)
+}
+
+func main() {
+	port := 3000
+
+	if sp := os.Getenv("SERVER_PORT"); sp != "" {
+		var err error
+		port, err = strconv.Atoi(sp)
+		if err != nil {
+			log.Fatal("Invalid port provided by env var SERVER_PORT: ", sp)
+		}
+	}
+
+	addr := fmt.Sprintf(":%d", port)
+	log.Printf("Starting server on address: %s\n\n", addr)
+
+	log.Fatal(http.ListenAndServe(addr, newMux()))
+}
